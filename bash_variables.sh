323012ab239f71971b7bb1769e92f170665cd524
Tim Sandberg <tasandberg@gmail.com>
2016-12-27 15:34:51 -0800

    Remove path setting from variables

--- bash_variables.sh
+++ bash_variables.sh
@@ -1,5 +1,3 @@
 export EDITOR="vim"
 export HISTSIZE=15000
 export CLICOLOR="auto"
-#Make sure set the correct PATH variable for you own machine
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
