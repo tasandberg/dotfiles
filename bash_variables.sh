51f9178dd3c661bfd73e82d010764ec86287c081
Tim Sandberg <tasandberg@gmail.com>
2017-03-30 12:01:23 -0700

    Add rbenv and fzf init to bash_variables

--- bash_variables.sh
+++ bash_variables.sh
@@ -1,4 +1,8 @@
-export EDITOR="vim"
+# Variables and some PATH setup
+export EDITOR="nvim"
 export HISTSIZE=15000
 export CLICOLOR="auto"
 export PATH="$PATH:$HOME/.dotfiles/bin"
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+eval "$(rbenv init -)"
