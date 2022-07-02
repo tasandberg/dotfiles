519aaebe0c31ed4e13bb93f0699c01b2d57ccaa9
Tim Sandberg <tasandberg@gmail.com>
2022-07-02 15:54:46 -0700

    Brew updates

--- bash_variables.sh
+++ bash_variables.sh
@@ -2,10 +2,16 @@
 export EDITOR="nvim"
 export HISTSIZE=15000
 export CLICOLOR="auto"
+export BASH_SILENCE_DEPRECATION_WARNING=1
 
 # Initialize Path
 PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
 export PATH
 PATH="$PATH:$HOME/.dotfiles/bin"
+
+# Initialize Brew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Initialize rbenv
 eval "$(rbenv init -)"
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
