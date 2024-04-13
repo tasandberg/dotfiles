25de09c39e72f4128f1e896ae22d63470fca99e5
Tim Sandberg <tasandberg@gmail.com>
2024-04-13 13:15:39 -0700

    Update prompt

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -54,4 +54,4 @@ case $OSTYPE in
 esac
 
 GIT_PS1_SHOWDIRTYSTATE=true
-PS1="\n$USER_COLOR\u@\H: $BRIGHT_YELLOW\w$BRIGHT_CYAN\$(__git_ps1 ' [%s]')\n$RESET\$ "
+PS1="$BRIGHT_CYAN\w$YELLOW\$(__git_ps1 ' [%s]')$BRIGHT_VIOLET\n❯ $RESET"
